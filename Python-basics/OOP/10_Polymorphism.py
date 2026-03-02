65ff9750575c90c9ec8faaaafa01147bfc096215
jing0728 <98197500+jing0728@users.noreply.github.com>
2026-03-02 09:29:16 -0500

    HeroFighter

--- Python-basics/OOP/10_Polymorphism.py
+++ Python-basics/OOP/10_Polymorphism.py
@@ -25,40 +25,102 @@
     2. 要有方法重写，不然多态无意义。
     3. 要有父类引用指向子类对象。
 """
-#1.定义动物类
-class Animal:    #抽象类（也叫：接口）
-    def speak(self): #抽象方法
-        print("动物会叫")
-# 2. 定义子类，狗类。
-class Dog(Animal):
-    def speak(self):
-        print('狗叫：汪汪汪')
+# #1.定义动物类
+# class Animal:    #抽象类（也叫：接口）
+#     def speak(self): #抽象方法
+#         print("动物会叫")
+# # 2. 定义子类，狗类。
+# class Dog(Animal):
+#     def speak(self):
+#         print('狗叫：汪汪汪')
 
 
-# 3. 定义子类，猫类。
-class Cat(Animal):
-    def speak(self):
-        print('猫叫：喵喵喵')
+# # 3. 定义子类，猫类。
+# class Cat(Animal):
+#     def speak(self):
+#         print('猫叫：喵喵喵')
 
-#汽车类
-class Car:
-    def speak(self):
-        print('汽车叫：嘟嘟嘟')
+# #汽车类
+# class Car:
+#     def speak(self):
+#         print('汽车叫：嘟嘟嘟')
 
-# 4. 定义函数，接收不同的动物对象，执行 speak 方法。
-def make_noise(an:Animal): #
-    an.speak()
+# # 4. 定义函数，接收不同的动物对象，执行 speak 方法。
+# def make_noise(an:Animal): #
+#     an.speak()
+# #5.测试
+# if __name__ == "__main__":
+#     # an:Animal = Dog()       #父类引用指向子类对象
+#     # d:Dog=Dog()     #创建狗类对象
+#     #5.1创建狗类，猫类对象
+#     d = Dog()
+#     c = Cat()
+#     #5.2演示多态
+#     make_noise(d)
+#     make_noise(c)
+#     #5.3 测试汽车类
+#     ca = Car()
+#     make_noise(ca)
+
+"""
+案例：演示Python的多态案例之 战斗平台。
+
+需求：
+
+    1. 构建对战平台（公共的函数）object_play()，接收：英雄机 和 敌机。
+    2. 在不修改对战平台代码的情况下，完成多次战斗。
+    3. 规则：
+        英雄机，1代战斗力为60，2代战斗力80
+        敌机，1代战斗力70
+
+代码提示：
+
+    英雄机1代 HeroFighter
+    英雄机2代 AdvHeroFighter
+    敌机 EnemyFighter
+"""
+#1.定义英雄机一代，战斗力六十
+class HeroFighter():
+    def power(self):
+        return 60
+
+#2.定义英雄机二代，战斗力八十
+class HeroFighter2(HeroFighter):
+    def power(self):
+        return 80
+
+#3.定义敌机，战斗力七十
+class EnmeyFighter():
+    def power(self):
+        return 70
+    
+#4.构建对战平台，公共的函数，接受不同的参数，有不同的效果
+def fight(hero:HeroFighter,enmey:EnmeyFighter):
+    if hero.power()>enmey.power():
+        print("英雄机 战胜 敌机")
+    else:
+        print("英雄机 惜败 敌机")
+        
 #5.测试
 if __name__ == "__main__":
-    # an:Animal = Dog()       #父类引用指向子类对象
-    # d:Dog=Dog()     #创建狗类对象
-    #5.1创建狗类，猫类对象
-    d = Dog()
-    c = Cat()
-    #5.2演示多态
-    make_noise(d)
-    make_noise(c)
-    #5.3 测试汽车类
-    ca = Car()
-    make_noise(ca)
-
+    #思路一不使用多态
+    #场景一：英雄机一代 vs 敌机
+    h1 = HeroFighter()
+    e1 = EnmeyFighter()
+    if h1.power() >= e1.power():
+        print("英雄机一代 战胜 敌机")
+    else:
+        print("英雄机一代 惜败 敌机")
+    #使用多态
+    fight(h1,e1)
+    #场景二：英雄机二代 vs 敌机
+    h2 = HeroFighter2()
+    e1 = EnmeyFighter()
+    if h2.power()>= e1.power():
+        print("英雄机二代 战胜 敌机")
+    else:
+        print("英雄机二代 惜败 敌机")
+     #使用多态
+    fight(h2,e1)
+    
+    #fight(h2,h1) #Python 是Duck Typing so有没有(hero:HeroFighter,enmey:EnmeyFighter)都会正常运行，只是会报警告而已
--- Python-basics/module.py
+++ Python-basics/module.py
@@ -28,4 +28,15 @@
 # if __name__=='__mian__':
 #     Data_Container_Project.subject_stats
 
-####软件包（package）
+####软件包（package）-->用来管理多个模块，包的本质也是一个模块
+#__init__.py -->用来解释这个包是干什么的
+#包的导入
+# #import 包名.模块名 —— import random.os -->调用方式：模块名，功能名,包名
+
+# #from 包名import * —— from random.rd import* -->调用方式：模块名，功能名
+#如果通过这个方式进行导入包下的文件，需要在__init__.py中添加__all__
+# #from 包名 import 模块名 —— from random import choice -->调用方式：模块名，功能名
+
+# #from 包名.模块名 import 功能名 — from random.rd import randint  -->调用方式：功能名
+
+# #from 包名.模块名 import * —— from random.rd import* -->调用方式：功能名
